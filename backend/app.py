c3021724a3e493b9f99054d64519ab34a46683cc
Vikas <vikasboggavarapu@outlook.com>
2026-08-04 16:44:28 +0530

    Initial commit

---
+++ backend/app.py
@@ -0,0 +1,92 @@
+from fastapi import FastAPI,UploadFile,File,Form
+from fastapi.middleware.cors import CORSMiddleware
+from pypdf import PdfReader
+from groq import Groq
+from dotenv import load_dotenv
+import os,io,json
+
+
+#1.Application setup
+
+load_dotenv()
+app = FastAPI()
+
+
+client = Groq(
+     api_key = os.getenv("GROQ_API_KEY ")
+)
+
+
+app.add_middleware (
+     CORSMiddleware,
+     allow_origins = ["*"],
+     allow_credentials = True,
+     allow_methods = ['*']
+)
+
+
+@app.get("/")
+def home():
+    return {"message" : "Resume Analyzer API"}
+
+#2.API endpoint
+@app.post("/analyze")
+async def upload_resume(file: UploadFile = File(...),job_description : str = Form(...)):
+
+#3.Read uploaded PDF
+    contents = await file.read()
+
+    pdf = PdfReader(io.BytesIO(contents))
+
+ #4.Extract text   
+    resume_text = ""
+
+    for page in pdf.pages:
+            page_text = page.extract_text() or ""
+            resume_text += page_text + "\n"
+
+#5.Pass it to LLM prompt
+    prompt = f"""
+     You are a resume analysis assistant.
+
+     Compare the following resume with the job description.
+
+     RESUME:
+     {resume_text}
+
+     JOB DESCRIPTION:
+     {job_description}
+
+     Return ONLY valid JSON in this format:
+
+     {{
+        "match_score": 0,
+        "matching_skills": [],
+        "missing_skills": [],
+        "suggestions": []
+    }}
+
+    match_score must be between 0 and 100.
+"""
+
+#6.Call the LLM
+
+    response = client.chat.completions.create(
+      model= "openai/gpt-oss-120b",
+      messages=[
+          {
+               "role" : "user",
+               "content" : prompt
+          }
+     ]
+)
+
+
+#7.get the LLM response
+
+    result = response.choices[0].message.content
+
+#8.Convert into JSON String 
+    analysis = json.loads(result)
+
+    return analysis
---
+++ frontend/src/App.jsx
@@ -0,0 +1,99 @@
+import { useState } from 'react'
+import "./App.css"
+
+function App() {
+     const [file,setFile] = useState(null);
+     const [jobDescription,setJobDescription] = useState("");
+     const [analysis,setAnalysis] = useState(null);
+
+     const analyzeResume = async ()=>{
+        if(!file){
+          alert("Upload a Resume!")
+          return
+        }
+
+        if(!jobDescription.trim()){
+          alert("Please enter a job description")
+        }
+
+        const formData = new FormData()
+
+        formData.append("file",file)
+        formData.append("job_description",jobDescription)
+
+        const response = await fetch(
+          "http://127.0.0.1:8000/analyze",
+          {
+            method : "POST",
+            body: formData
+          }
+        )
+
+        const data = await response.json();
+
+        setAnalysis(data);
+     }
+
+  return (
+    <div className= "container">
+   <h1>AI Resume Analyzer</h1>
+   <h3>Upload Resume</h3>
+
+   <input
+    type='file'
+    accept='.pdf'
+    onChange={(e) => setFile(e.target.files[0])}
+   />
+
+   <h3>Job Description</h3>
+
+<textarea
+  rows = "10"
+  cols= "60"
+  placeholder='Paste the job description'
+  value={jobDescription}
+  onChange={(e)=> setJobDescription(e.target.value)}
+/>
+
+<br/>
+<br/>
+
+<button onClick={analyzeResume} >
+  Analyze Resume
+</button>
+
+
+{analysis && (
+  <div>
+    <h2>Analysis Result</h2>
+
+    <h2>Match Score</h2>
+    <p>{analysis.match_score}%</p>
+
+    <h3>Matching Skills</h3>
+    <ul>
+      {analysis.matching_skills.map((skill, index) => (
+        <li key={index}>{skill}</li>
+      ))}
+    </ul>
+
+    <h3>Missing Skills</h3>
+    <ul>
+    {analysis.missing_skills.map((skill, index) => (
+        <li key={index}>{skill}</li>
+      ))}
+    </ul>
+
+    <h3>What to improve</h3>
+    <ul>
+      {analysis.suggestions.map((suggestion,index)=>(
+        <li key={index}>{suggestion}</li>
+      ))}
+    </ul>
+    </div>
+)}
+   </div>
+  )
+}
+
+export default App
